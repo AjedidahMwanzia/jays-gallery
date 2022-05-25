1ad66f8501dcebd75465e2251eb9b317f974723f
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-05-25 23:57:09 +0300

    define the url path for the projects

--- photos/urls.py
+++ photos/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('',views.gallery ,name= 'gallery')
+    path('photo/<str:pk>',views.viewPhoto ,name= 'photo')
+    path('add/',views.addPhoto ,name= 'add')
 ]
