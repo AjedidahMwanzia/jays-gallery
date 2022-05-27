355ed2f37bcf4a732845b918c575b36477e549d3
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-05-27 14:25:45 +0300

    add command to query category

--- photos/views.py
+++ photos/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-
+from .models import Category,Photo
 # Create your views here.
 def gallery(request):
-    return render(request,'photos/gallery.html')
+    categories = categories.objects.all()
+    context = {'categories':categories}
+    return render(request,'photos/gallery.html',context)
 
 def viewPhoto(request,pk):
     return render(request,'photos/photo.html')
